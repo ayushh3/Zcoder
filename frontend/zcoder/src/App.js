024e419f7177ee1bc2a14c0d2c86ec9de17dcbe9
Ayush_Sahu <ayushsahu3312@gmail.com>
2024-06-04 00:42:28 +0530

    commit-3/ayush

--- frontend/zcoder/src/App.js
+++ frontend/zcoder/src/App.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
-import {
-  BrowserRouter as Router, 
-  Routes, 
-  Route,
-  Navigate,
-} from 'react-router-dom';
-
 import Zcoderr from './components/Zcoderr';
 import ChatRoom from './components/ChatRoom';
 
@@ -16,9 +9,12 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Header/>
+        <Header />
         <Routes>
           <Route path="/" element={<Zcoderr />} />
+          <Route path="/chat" element={<ChatRoom />} />
+          {/* Redirect to home if no match */}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
     </div>
@@ -26,4 +22,5 @@ function App() {
 }
 
 export default App;
+
 
